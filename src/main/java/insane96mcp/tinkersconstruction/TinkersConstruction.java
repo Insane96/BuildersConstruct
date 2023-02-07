2428d22cf613f30a8c60a18a0a2917f9934e5886
Alberto <delvillano.alberto@gmail.com>
2023-02-07 15:52:29 +0100

    First Commit

---
+++ src/main/java/insane96mcp/tinkersconstruction/TinkersConstruction.java
@@ -0,0 +1,12 @@
+package insane96mcp.tinkersconstruction;
+
+@Mod(TinkersConstruction.MOD_ID)
+public class TinkersConstruction
+{
+    public static final String MOD_ID = "tinkersconstruction";
+    public static final String RESOURCE_PREFIX = MOD_ID + ":";
+
+    public TinkersConstruction() {
+        //ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.COMMON, Config.COMMON_SPEC);
+    }
+}
